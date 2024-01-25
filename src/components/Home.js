0996f0029301be1654860f1d334df7ca75ded9dc
attadasuresh <sureshattada12233@gmail.com>
2024-01-26 03:54:08 +0530

    newone

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,11 +19,11 @@ const Home = () => {
 <p className='para5'>THE #1 RATED WORKFORCE PLATFORM</p>
 <h1 className='head5'>Explore  Seamless Staff Hub by product</h1>
 </div>
-
+<div className='d-flex flex-row' style={{ margin:"50px"}}>
 <div className='card7'>
     <div className='card8'>
     <img className='img3' src="images\icone-de-nuage-orange.png"/>
-<h1 style={{fontSize:"25px",fontWeight:"bold"}}>Hr Cloud</h1>
+<h1 style={{fontSize:"25px",fontWeight:"bold",margin:"10px"}}>Hr Cloud > </h1>
 <p className='para8'>manage and automate employee lifecycle Award-winning HR software built to help</p>
 
 <div className='d-flex flex-row'>
@@ -33,13 +33,105 @@ const Home = () => {
     <p style={{paddingLeft:"10px",fontWeight:"bold"}}>Payroll</p>
 </div>
 <div className='d-flex flex-row card9' >
+    <img className='img5' src="images\pngtree-euro-business-finance-logo-icon-template-png-image_5453467.png"/>
+    <p style={{paddingLeft:"10px",fontWeight:"bold"}}>Benfits</p>
+</div>
+</div>
+<div className='d-flex flex-row'>
+
+<div className='d-flex flex-row card9' >
+    <img className='img5' src="images\49117.png"/>
+    <p style={{paddingLeft:"10px",fontWeight:"bold"}}>T & A</p>
+</div>
+<div className='d-flex flex-row card9' >
+    <img className='img5' src="images\4415875.png"/>
+    <p style={{paddingLeft:"10px",fontWeight:"bold"}}>Recruting</p>
+</div>
+</div>
+<div className='d-flex flex-row'>
+
+<div className='d-flex flex-row card9' >
+    <img className='img5' src="images\simple-people-icon-icon-of-grouping-of-people-png.webp"/>
+    <p style={{paddingLeft:"10px",fontWeight:"bold"}}>Pulse</p>
+</div>
+<div className='d-flex flex-row card9' >
+    <img className='img5' src="images\131-1318263_group-people-communication-message-talk-comments-people-management.png"/>
+    <p style={{paddingLeft:"10px",fontWeight:"bold"}}>PEO</p>
+</div>
+</div>
+</div>
+
+</div>
+<div className='card71'>
+    <div className='card8'>
+    <img className='img3' src="images\icone-de-nuage-orange.png"/>
+<h1 style={{fontSize:"25px",fontWeight:"bold",margin:"10px"}}>IT Cloud > </h1>
+<p className='para8'>Easily set up, secure, and manage your people's devices and apps-100% remotely.</p>
+
+<div className='d-flex flex-row'>
+
+<div className='d-flex flex-row card91' >
+
     <img className='img5' src="images\dollar-coin-outline-icon.webp"/>
-    <p style={{paddingLeft:"10px",fontWeight:"bold"}}>Payroll</p>
+    <p style={{paddingLeft:"10px",fontWeight:"bold"}}>App Management</p>
+
+</div>
+
+</div>
+<div className='d-flex flex-row'>
+
+<div className='d-flex flex-row card91' >
+<img className='img5' src="images\dollar-coin-outline-icon.webp"/>
+    <p style={{paddingLeft:"10px",fontWeight:"bold"}}>Device Management</p>
+</div>
+
+</div>
+<div className='d-flex flex-row'>
+
+<div className='d-flex flex-row card91' >
+    <img className='img5' src="images\dollar-coin-outline-icon.webp"/>
+    <p style={{paddingLeft:"10px",fontWeight:"bold"}}>Inventory Management</p>
+</div>
+
 </div>
 </div>
 
 </div>
+<div className='card71'>
+    <div className='card8'>
+    <img className='img3' src="images\icone-de-nuage-orange.png"/>
+<h1 style={{fontSize:"25px",fontWeight:"bold",margin:"10px"}}>Finance Cloud ></h1>
+<p className='para8'>Manage and report on all of your company's spend-in one place</p>
 
+<div className='d-flex flex-row'>
+
+<div className='d-flex flex-row card91' >
+
+    <img className='img5' src="images\dollar-coin-outline-icon.webp"/>
+    <p style={{paddingLeft:"10px",fontWeight:"bold"}}>Corporate Cards</p>
+
+</div>
+
+</div>
+<div className='d-flex flex-row'>
+
+<div className='d-flex flex-row card91' >
+<img className='img5' src="images\dollar-coin-outline-icon.webp"/>
+    <p style={{paddingLeft:"10px",fontWeight:"bold"}}>Expense Management</p>
+</div>
+
+</div>
+<div className='d-flex flex-row'>
+
+<div className='d-flex flex-row card91' >
+    <img className='img5' src="images\dollar-coin-outline-icon.webp"/>
+    <p style={{paddingLeft:"10px",fontWeight:"bold"}}>Bill pay</p>
+</div>
+
+</div>
+</div>
+
+</div>
 </div>
     </>
   )
