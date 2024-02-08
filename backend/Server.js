0f627b3921563789cf21d988228cd63e4bdca9ab
attadasuresh <sureshattada12233@gmail.com>
2024-02-09 04:20:36 +0530

    bk

---
+++ backend/Server.js
@@ -0,0 +1,74 @@
+const express = require ("express");
+const cors = require ("cors");
+const mysql = require ("mysql");
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"crud"
+})
+
+app.get("/",(req,res)=>{
+ 
+    const sql = "SELECT * FROM student";
+    db.query(sql, (err, data)=>{
+ if(err)
+    return res.json("Error");
+    return res.json(data);
+ 
+    })
+
+})
+ 
+
+
+app.delete('/student/:id',(req,res)=>{
+ 
+    const sql = "DELETE FROM student WHERE ID = ?";
+    const id = req.params.id;
+    db.query(sql, [id], (err, data)=>{
+ if(err) return res.json("Error");
+    return res.json(data);
+    })
+})
+
+
+app.post('/create',(req,res) =>{
+    const sql = "INSERT INTO student  (`Name`,`Jobposition`,`JobExperience`,`Location`,`currentSalary`,`country`,`email`,`contactNumber`,`howknow`) VALUES (?) ";
+const values =[
+
+    req.body.Name,
+    req.body.Jobposition,
+    req.body.JobExperience,
+    req.body.Location,
+    req.body.currentSalary,
+    req.body.country,
+    req.body.email,
+    req.body.Contactnumber,
+    req.body.Howknow,
+
+
+]
+
+db.query(sql,[values],(err,data)=>{
+
+    if(err) return res.json(err);
+    return res.json(data);
+})
+
+
+})
+
+
+app.listen(8081,()=>{
+    console.log("listining");
+})
