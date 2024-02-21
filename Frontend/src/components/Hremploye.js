a071545806a2fb6befe3056258a0f2675daf4979
attadasuresh <sureshattada12233@gmail.com>
2024-02-22 00:47:42 +0530

    data

--- Frontend/src/components/Hremploye.js
+++ Frontend/src/components/Hremploye.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import './Hremploye.css'
+import {Link}  from 'react-router-dom'
 
 const Hremploye = () => {
 
@@ -19,31 +21,59 @@ console.log(employe);
   <div>
 
 <ul>
-   {
+  <h1 className='Hre-head1'>EMPLOYEE DATA</h1>
+<div className='Hre-card1'>
+  <button className='Hre-button1'>TOTAL - EMPLOYES<p>0</p></button>
+  <button className='Hre-button1'>PRESENT - EMPLOYES<p>0</p></button>
+  <button className='Hre-button1'>ABSENT - EMPLOYES<p>0</p></button>
+  <button className='Hre-button1'>EMPLOYES - DETAILS <p>{employe.length}</p></button>
+  </div>
+   
+  
+<Link to='/EmployeRegister' className='Hre-head2'>ADD - EMPLOYEE </Link>
+<div  className='Hre-table1'>
+<table>
+  <tr>
+    <th>NAME</th>
+    <th>EMPLOYE ID</th>
+    <th>JOBPOSITION</th>
+    <th>LOCATION</th>
+    <th>DATEOFBIRTH</th>
+    <th>EMAIL</th>
+    <th>NUMBER</th>
+    <th>ADDRESS</th>
+    <th>ACTIONS</th>
 
+
+  </tr>
+  {
 employe.map((data)=>(
-<li key={data.id} >
-<h1>employee data</h1>
+  <tr>
+    <td>{data.name}</td>
+    <td>{data.userid}</td>
+    <td>{data.jobposition}</td>
+    <td>{data.location}</td>
+    <td>{data.dateofbirth}</td>
+    <td>{data.email}</td>
+    <td>{data.number}</td>
+    <td>{data.address}</td>
+     <td><button className='Hre-button3'>DELETE</button></td>  
 
-<p>name:{data.name}</p>
-<p>{data.userid}</p>
-<p>{data.jobposition}</p>
-<p>{data.location}</p>
-<p>{data.dateofbirth}</p>
-<p>{data.email}</p>
-<p>{data.number}</p>
-<p>{data.address}</p>
-
-</li>
-)
-
-
-
+  </tr>
+))}
+</table>
+</div>
 
 
 
-)
-   }
+
+
+
+
+
+
+
+   
 
 
 
